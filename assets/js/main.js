06428137d6f9c8a52f470e05342e7f794c776d9e
Hala Madi <Halamadi75@gmail.com>
2024-03-11 02:28:29 +0200

    task

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var tableBody = document.querySelector('tbody');
+
+//     for (var i = 0; i < 11; i++) {
+//         var row = document.createElement('tr');
+//         row.innerHTML = `
+//             <td>Name name</td>
+//             <td>
+//                 <img
+//                     src="/assets/img/student.png"
+//                     alt="A student takes an exam"
+//                     width="80px"
+//                     height="60px"
+//                 />
+//             </td>
+//             <td>MD1</td>
+//             <td>online</td>
+//             <td>08:30 AM</td>
+//             <td>00:31:05</td>
+//             <td>80</td>
+//             <td>75%</td>
+//             <td>82%</td>
+//             <td>Actions</td>
+//         `;
+//         tableBody.appendChild(row);
+//     }
+// });
+
